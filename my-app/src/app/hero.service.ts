7c0c12f914923fd58214e6ba460aff0f6a18e660
Rafael Castillo <rafaelcastilloalcaraz@gmail.com>
2019-09-16 22:53:24 +0200

    5. Routing

--- my-app/src/app/hero.service.ts
+++ my-app/src/app/hero.service.ts
@@ -3,7 +3,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
- 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,5 +15,11 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
